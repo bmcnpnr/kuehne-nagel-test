e83ec6da8549a359d02f7ab5657ae2a1ad767377
Burak Mete CANPINAR <m.canpinar@emakina.com.tr>
2022-07-31 14:05:29 +0300

    added basic authentication mechanism

--- wallet-service-server/src/main/java/com/kuehne/nagel/config/SecurityConfiguration.java
+++ wallet-service-server/src/main/java/com/kuehne/nagel/config/SecurityConfiguration.java
@@ -1,14 +1,17 @@
 package com.kuehne.nagel.config;
 
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter implements ApplicationContextAware {
 
     private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
@@ -19,6 +22,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .withUser("kuehne.nagel")
                 .password(passwordEncoder().encode("secret"))
                 .roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.csrf().disable();
     }
 
     @Bean
