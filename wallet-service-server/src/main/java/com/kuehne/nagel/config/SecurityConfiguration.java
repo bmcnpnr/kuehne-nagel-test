2ede781c8c85c95a208d3be6a4e77163b921a1f4
Burak Mete CANPINAR <m.canpinar@emakina.com.tr>
2022-07-31 17:44:51 +0300

    configured h2 console to work with spring security and Docker

--- wallet-service-server/src/main/java/com/kuehne/nagel/config/SecurityConfiguration.java
+++ wallet-service-server/src/main/java/com/kuehne/nagel/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -27,6 +28,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter implemen
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
         http.csrf().disable();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+        web.ignoring().antMatchers("/h2-console/**");
     }
 
     @Bean
